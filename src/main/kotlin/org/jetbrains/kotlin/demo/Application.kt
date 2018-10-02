11a03097f34e3df5bb75196584431500edc77b44
Likhanov Maxim <Maksim_Likhanov@epam.com>
2018-10-03 00:21:48 +0300

    adding frontend

--- src/main/kotlin/org/jetbrains/kotlin/demo/Application.kt
+++ src/main/kotlin/org/jetbrains/kotlin/demo/Application.kt
@@ -2,10 +2,15 @@ package org.jetbrains.kotlin.demo
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.ImportResource
 
 @SpringBootApplication
+//@ImportResource("classpath:app-config.xml")
 class Application
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
 }
+
+//https://www.baeldung.com/maven-webjars
+//https://www.webjars.org/documentation#springboot
